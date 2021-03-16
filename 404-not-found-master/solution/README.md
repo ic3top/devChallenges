f1e0fb6003f6416fe4045cd49a4df4bf80a01c20
ic3top <potatai123@gmail.com>
2021-03-16 18:01:51 +0200

    upload 2tasks

---
+++ 404-not-found-master/solution/README.md
@@ -0,0 +1,14 @@
+# 404 not found page
+> Visit it on [GHpages](#)  
+
+## General info
+I found this article pritty useful: (Creating a custom 404 page for your GitHub Pages site)[https://docs.github.com/en/github/working-with-github-pages/creating-a-custom-404-page-for-your-github-pages-site]  
+You can find same FIGMA template [here.](https://www.figma.com/file/QeKWLNhB13zDjJzqR22TKE/404-page-challenge)
+
+
+## Overview
+![screenshot](./screenshots/demo.jpg)
+![screenshot-mobile](./screenshots/demo-mob.jpg)
+
+## Status
+Project is: _finished_
---
+++ portfolio-master/solution/src/js/script.js
@@ -0,0 +1,63 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const bars = document.querySelectorAll('.skills__bar');
+
+  const [...projects] = document.querySelectorAll('.portfolio__block');
+  const portfolio = document.querySelector('.portfolio__wrapper');
+
+  const [...buttons] = document.querySelectorAll('.projects .btn');
+  const [reactElements, vueElements, responsiveElements] = sort(projects);
+  responsiveElements.forEach((el) => el.style.display = 'block');
+
+  buttons.forEach((el) => {
+    el.addEventListener('click', () => {
+      buttons.forEach((el) => el.classList.remove('btn_bg-blue'));
+      el.classList.add('btn_bg-blue');
+      if (el.dataset.type === 'react') {
+        insertProjects(reactElements);
+      } else if (el.dataset.type === 'vue') {
+        insertProjects(vueElements);
+      } else {
+        insertProjects(responsiveElements);
+      }
+    });
+  });
+
+  function setWidth(el, width) {
+    el.style.width = width;
+  }
+
+  setWidth(bars[0], '186px');
+  setWidth(bars[1], '236.85px');
+  setWidth(bars[2], '236.85px');
+  setWidth(bars[3], '236.85px');
+  setWidth(bars[4], '203px');
+  setWidth(bars[5], '208px');
+
+  function sort(arr) {
+    const react = [];
+    const vue = [];
+    const responsive = [];
+
+    arr.forEach((el) => {
+      el.style.display = 'none';
+      if (el.dataset.type == 'responsive') {
+        responsive.push(el);
+      } else if (el.dataset.type == 'vue') {
+        vue.push(el);
+      } else {
+        react.push(el);
+      }
+    });
+    return [react, vue, responsive];
+  }
+
+  function insertProjects(arr) {
+    reactElements.forEach((el) => el.style.display = 'none');
+    vueElements.forEach((el) => el.style.display = 'none');
+    responsiveElements.forEach((el) => el.style.display = 'none');
+    arr.forEach((el) => {
+      el.style.display = 'block';
+      portfolio.appendChild(el);
+    });
+  }
+});
